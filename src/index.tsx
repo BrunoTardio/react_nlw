a4f7c134d86ca391aba853fb3db270850e965905
Bruno <brunotardioti@gmail.com>
2021-06-24 10:51:54 -0300

    SLW 03

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+import './services/firebase';
+
+import './styles/global.scss';//importa o css
+
+ReactDOM.render(// método usado uma única vez n aaplicação, exibe algo dentro do elemento no html
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')//vai no index procura o documento 'root' e exibe tudo que está no <App/>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
